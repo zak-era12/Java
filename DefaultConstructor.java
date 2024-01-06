1c8b9e68df39742bb2e117579dfbc2abdc45eabf
Zakera <91636007+zak-era12@users.noreply.github.com>
2024-01-06 09:54:02 +0530

    DefaultConstructor.java

--- DefaultConstructor.java
+++ DefaultConstructor.java
@@ -2,10 +2,14 @@ class DefaultConstructor {
  
 int num;
 boolean flag;
+DefaultConstructor()
+ {
+  //Empty Constructor
+ }
 public static void main(String[] args) {        
  
          // A default constructor is called        
-        Main obj = new Main();   
+        DefaultConstructor obj = new DefaultConstructor();   
  
 System.out.println("num:default value = " + obj.num);
 System.out.println("flag:default value = " + obj.flag);
