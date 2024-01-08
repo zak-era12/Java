6ac9001a6840267c2b82989c9b53c45890d3b3f0
Zakera <91636007+zak-era12@users.noreply.github.com>
2024-01-09 00:44:22 +0530

    Add files via upload

---
+++ StaticMethodExample.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void staticMethod() {
+        System.out.println("Hello, from staticMethod!");
+    }
+}
+
+public class StaticMethodExample {
+    public static void main(String[] args) {
+        // Call the static method
+        MyClass.staticMethod();
+    }
+}
